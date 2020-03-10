753042394c3b83f715ad2a53bff5f8e72362af5f
DanGam96DM <35945144+DanGam96DM@users.noreply.github.com>
2020-03-10 06:08:12 +0100

    concexion con mongoose y creacion de rutas

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express=require('express');
+const connectDB=require('./config/db');
 const app=express();
+connectDB();
 app.get('/', (req, res)=>res.send('API Corriendo'));
+
+//Se definen todas las rutas
+app.use('/api/users', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`El servidor se inicio en el puerto ${PORT}`));
